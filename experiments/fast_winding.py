12d03e21bcb165d857bfbeec242496e8b8b28dd5
Ben Pedigo <benjamindpedigo@gmail.com>
2025-02-27 16:46:50 -0800

    stash demo

---
+++ experiments/fast_winding.py
@@ -0,0 +1,83 @@
+# /// script
+# requires-python = ">=3.12,<3.13"
+# dependencies = [
+#     "caveclient[cv]==7.6.0",
+#     "gpytoolbox==0.3.3",
+#     "pyvista[all]==0.44.2",
+# ]
+# ///
+
+
+# %%
+import numpy as np
+import pyvista as pv
+from caveclient import CAVEclient
+from gpytoolbox import fast_winding_number
+
+client = CAVEclient("minnie65_phase3_v1", version=1300)
+
+cv = client.info.segmentation_cloudvolume()
+
+# %%
+root_id = 864691135662877004
+
+leaves = client.chunkedgraph.get_leaves(root_id, stop_layer=3)
+
+node = leaves[0]
+node = root_id
+mesh = cv.mesh.get(
+    node, remove_duplicate_vertices=True, deduplicate_chunk_boundaries=False
+)[node]
+
+mesh = (mesh.vertices, mesh.faces)
+
+# %%
+
+bounds = np.array((mesh[0].min(axis=0), mesh[0].max(axis=0)))
+
+n_samples = 100000
+
+indices = np.random.choice(len(mesh[0]), n_samples, replace=True)
+points = mesh[0][indices].copy()
+points += np.random.normal(0, 100, points.shape)
+
+# %%
+
+out = fast_winding_number(points, mesh[0], mesh[1])
+
+# %%
+
+plotter = pv.Plotter(shape=(1, 2))
+
+plotter.subplot(0, 0)
+plotter.add_mesh(pv.make_tri_mesh(*mesh), opacity=0.5)
+
+plotter.add_points(
+    points,
+    scalars=out,
+    render_points_as_spheres=True,
+    point_size=2,
+    cmap="coolwarm",
+    clim=[0, 1],
+    show_scalar_bar=False,
+)
+
+plotter.subplot(0, 1)
+
+plotter.add_mesh(pv.make_tri_mesh(*mesh), opacity=0.5)
+
+plotter.add_points(
+    points[out > 0.5],
+    scalars=out[out > 0.5],
+    render_points_as_spheres=True,
+    point_size=2,
+    cmap="coolwarm",
+    clim=[0, 1],
+    show_scalar_bar=False,
+)
+
+plotter.link_views()
+plotter.enable_fly_to_right_click()
+
+plotter.export_html("fast_winding.html")
+plotter.show()
